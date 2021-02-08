24dd6762185d02828e506db1dd73a2f4a56eeda1
EamonYin <xiaominglovejava@163.com>
2021-02-08 16:39:34 +0800

    RedisTesting~

--- src/main/java/com/eamondocker/mysqltest/controller/testController.java
+++ src/main/java/com/eamondocker/mysqltest/controller/testController.java
@@ -4,10 +4,15 @@ import com.eamondocker.mysqltest.entity.User;
 
 import com.eamondocker.mysqltest.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 /**
@@ -16,10 +21,13 @@ import java.util.List;
  * @version:1.0
  */
 @RestController
-public class testController {
+public class testController implements Serializable {
 
     @Autowired
     UserMapper userMapper;
+
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @GetMapping("/hello")
     public String test(){
@@ -30,5 +38,23 @@ public class testController {
     public List<User> getUser(){
         List<User> users = userMapper.selectList(null);
         return users;
+    }
+
+    @GetMapping("/getViewNum")
+    public String getViewNum(){
+
+        //获得浏览量
+        Long views = redisTemplate.opsForValue().increment("Views");
+        //获取当前IP
+        InetAddress ip4 = null;
+        try {
+            ip4 = InetAddress.getByName(Inet4Address.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        redisTemplate.opsForValue().set("IP",ip4);
+
+        return "当前浏览量为："+views+"次"+" 当前IP："+ip4;
     }
 }
